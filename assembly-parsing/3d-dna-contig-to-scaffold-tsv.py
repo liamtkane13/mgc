f749b290b7af27ea56220a623548bd6eafd3760e
Stephen McLaughlin <steve.mclaughlin@medicinalgenomics.com>
2021-11-30 15:01:31 -0500

    steve's python version

--- assembly-parsing/3d-dna-contig-to-scaffold-tsv.py
+++ assembly-parsing/3d-dna-contig-to-scaffold-tsv.py
@@ -9,39 +9,42 @@ parser = argparse.ArgumentParser(description='Open an assembly file and map cont
 parser.add_argument('-i', '--infile', help='Assembly file to map', required=True,dest='infile')
 args = parser.parse_args()
 
-contig_dict = {}
-scaffold_list = []
-
 def parse_contigs():
-    counter = 0
+    scaffold_list = []
+    contig_dict = {}
+    contig_length = {}
     with open(args.infile, 'r') as infile:
         for lines in infile:
             line = lines.rstrip('\n')
             if line.startswith('>'):
                 contig_array = line.strip('>').split(' ')
                 name = contig_array[0]
-                number = contig_array[1]
-                length = contig_array[2]
-                contig_dict[number] = name 
-                
+                number = int(contig_array[1])
+                length = int(contig_array[2])
+                contig_dict[number] = name
+                contig_length[number] = length
             else:
                 scaffolds = line.split(' ')
+                scaffolds = list(map(int, scaffolds)) #turn list into list of integers from list of strings
                 scaffold_list.append(scaffolds)
-                counter +=1
+    return contig_dict, contig_length, scaffold_list
 
-
-
-def align_scaffolds(dict, list):
-    for item in list:
-        if item in dict.keys():
-            print(item)
+def align_scaffolds(contig_dict, contig_length, list):
+    scaffold_number = 0
+    for l in list:
+        scaffold_number += 1
+        scaffold_name = "%s_%s"%('HiC_scaffold', scaffold_number)
+        for c in l:
+            contig_name = contig_dict[abs(c)]
+            strand = '+'
+            if c < 0:
+                strand = '-'
+            print("%s\t%s\t%s\t%s"%(scaffold_name, contig_name, strand, contig_length[abs(c)]))
     
 
 def main():
-    parse_contigs()
-#    print(contig_dict)
-#    print(scaffold_list)
-    align_scaffolds(contig_dict, scaffold_list)
+    contig_dict, contig_length, scaffold_list = parse_contigs()
+    align_scaffolds(contig_dict, contig_length, scaffold_list)
 
 if __name__ == '__main__':
     main()
