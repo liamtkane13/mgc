4890cba46c1d4ea2fe4eb469e368b23a353391c1
Liam Kane <liamkane@MGC-MACBOOK-PRO-2.local>
2022-05-19 16:23:47 -0400

    added file construction

--- qc_project/pull_s3_qualimap/pull_s3_qualimap.py
+++ qc_project/pull_s3_qualimap/pull_s3_qualimap.py
@@ -2,16 +2,23 @@
 
 # pull_s3_qualimap.py
 
-import os 
 import subprocess
 
+def make_accession_files():
+    todo_public_bam_process = (f'aws s3 ls s3://mgcdata/SS2/bams/public/ > todo_bam_list.txt')
+    todo_private_bam_process = (f'aws s3 ls s3://mgcdata/SS2/bams/private/ >> todo_bam_list.txt')
+    done_bam_process = (f'aws s3 ls s3://mgcdata/SS2/qualimap/ > done_bam_list.txt')
+    subprocess.check_output(['bash', '-c', todo_public_bam_process])
+    subprocess.check_output(['bash', '-c', todo_private_bam_process])
+    subprocess.check_output(['bash', '-c', done_bam_process])
 
 
 def make_rsp_list():
 
     bam_list = []
+    finished_bam_list = []
 
-    with open('public_bam_list.txt') as file:
+    with open('todo_bam_list.txt') as file:
         for line in file:
             if "bai" in line:
                 continue
@@ -19,7 +26,7 @@ def make_rsp_list():
                 bam_info = line.split(' ')
                 for i in bam_info:
                     if "bam" in i:
-                        it = i.strip('\n')
+                        it = i.strip('.bam\n')
                         bam_list.append(it)
     return bam_list
 
@@ -28,25 +35,29 @@ def run_qualimap(sample_list):
 
     for i in sample_list:
 
-        download_process = (f'aws s3 cp s3://mgcdata/SS2/bams/public/{i} .') 
+        download_process = (f'aws s3 cp s3://mgcdata/SS2/bams/public/{i}.bam .') 
         subprocess.check_output(['bash', '-c', download_process])
 
-        i_name = i.split('.')[0]
-        qualimap_process = (f'qualimap bamqc -bam {i} -outdir {i_name} -outformat HTML -nt 4')
+        qualimap_process = (f'qualimap bamqc -bam {i}.bam -outdir {i} -outformat HTML -nt 4')
         subprocess.check_output(['bash', '-c', qualimap_process])
 
-        sync_process = (f'aws s3 sync {i_name} s3://mgcdata/SS2/qualimap/')
+        sync_process = (f'aws s3 sync {i} s3://mgcdata/SS2/qualimap/')
         subprocess.check_output(['bash', '-c', sync_process])
 
-        delete_process = (f'rm {i_name}*')
+        delete_process = (f'rm {i}*')
         subprocess.check_output(['bash', '-c', delete_process])
+
+
+        print('quit')
+        quit()
 
         
 
 
 def main():
-    bam_list = make_rsp_list()
-    run_qualimap(bam_list)
+    make_accession_files()
+#    bam_list = make_rsp_list()
+#    run_qualimap(bam_list)
 
 
 
