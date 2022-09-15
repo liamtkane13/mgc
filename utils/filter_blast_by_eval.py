0a8c9b7349636b5658dfba93ef047d1c46c15b0f
Liam Kane <liamkane@MGC-MACBOOK-PRO-2.local>
2022-09-15 16:37:52 -0400

    successfully printing desired output to terminal

--- utils/filter_blast_by_eval.py
+++ utils/filter_blast_by_eval.py
@@ -12,25 +12,49 @@ parser.add_argument('-e', '--e_value', help = 'Maximum e-value to filter', requi
 parser.add_argument('-n', '--num_hits', help = 'Number of hits requested', required = True, dest = 'num_hits')
 args = parser.parse_args()
 
-def parse_blast_files(files, eval, numhits):
+
+def sort_key(list_name):
+    return list_name[10]
+
+
+def parse_blast_files(files, eval):
+    
     blast_list = []
-    evals_list = []
+    
     for file in files:
         with open(file, 'r') as file:
+
             for line in file:
                 linecomp = line.split('\t')
-                blast_list.append(linecomp)
                 e_val = linecomp[10]
                 e_val = float(e_val)
+
                 if e_val < float(eval):
-                    print(e_val)
+                    filtered_e_val = str(e_val)
+
+                    if filtered_e_val in linecomp:
+                        blast_list.append(linecomp)
+
+            return blast_list
+
+
+
+def sort_and_subsample_results(data_list, numhits):
+    data_list.sort(key = sort_key)
+    top_hits_requested = data_list[0:numhits]
+    print(top_hits_requested)
+
+
+
 
 
 def main():
     infiles = args.infiles
     e_value = args.e_value
-    num_hits = args.num_hits
-    parse_blast_files(infiles, e_value, num_hits)
+    num_hits = int(args.num_hits)
+    blast_list = parse_blast_files(infiles, e_value)
+    sort_and_subsample_results(blast_list, num_hits)
+    
 
 if __name__ == '__main__':
     main()
