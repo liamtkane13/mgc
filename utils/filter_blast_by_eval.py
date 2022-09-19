831993a4f7f376d42c90fb56aa2ab4a4709ecb08
Liam Kane <liamkane@MGC-MACBOOK-PRO-2.local>
2022-09-19 10:46:17 -0400

    filtering the IDs correctly; need to limit to just top hit

--- utils/filter_blast_by_eval.py
+++ utils/filter_blast_by_eval.py
@@ -20,30 +20,38 @@ def sort_key(list_name):
 def parse_blast_files(files, eval):
     
     blast_list = []
+    query_id_list = []
     
     for file in files:
         with open(file, 'r') as file:
 
             for line in file:
                 linecomp = line.split('\t')
+                query_id = linecomp[0]
                 e_val = linecomp[10]
                 e_val = float(e_val)
+                
+                if query_id not in query_id_list:
+                    query_id_list.append(query_id)
 
                 if e_val < float(eval):
                     filtered_e_val = str(e_val)
 
                     if filtered_e_val in line:
                         blast_list.append(line)
-
-            return blast_list
-
+            return blast_list, query_id_list
 
 
-def sort_and_subsample_results(data_list, numhits):
-    data_list.sort(key = sort_key)
-    top_hits_requested = data_list[0:numhits]
-    for i in top_hits_requested:
-        print(i)
+
+def sort_and_subsample_results(data_list1, data_list2, numhits):
+    data_list1.sort(key = sort_key)
+    top_hits_requested = data_list2[0:numhits]
+#    for i in top_hits_requested:
+#        print(i)
+    for line in data_list1:
+        for i in top_hits_requested:
+            if i in line:
+                print(line)
 
 
 
@@ -53,8 +61,8 @@ def main():
     infiles = args.infiles
     e_value = args.e_value
     num_hits = int(args.num_hits)
-    blast_list = parse_blast_files(infiles, e_value)
-    sort_and_subsample_results(blast_list, num_hits)
+    blast_list, query_id_list = parse_blast_files(infiles, e_value)
+    sort_and_subsample_results(blast_list, query_id_list, num_hits)
     
 
 if __name__ == '__main__':
