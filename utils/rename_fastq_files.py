98d6ddd0335ea861cc17643db3fdcfed6a96ab97
Liam Kane <liamkane@MGC-MACBOOK-PRO-2.local>
2022-09-29 11:28:01 -0400

    silly script for renaming files

---
+++ utils/rename_fastq_files.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from subprocess import check_output as run
+from glob import glob
+
+
+def rename_files(files):
+    for file in files:
+        sample_name = file.split('_')[0]
+        suffix = file.split('_')[1]
+        if '1' in suffix:
+            command_1 = (f'mv {file} {sample_name}_R1_001.fastq')
+            run(['bash', '-c', command_1])
+        if '2' in suffix:
+            command_2 = (f' mv {file} {sample_name}_R2_001.fastq')
+            run(['bash', '-c', command_2])
+
+def main():
+    infiles = '/Dragen/Smart_photoperiod/Smart_photosensitive/*'
+    infiles = glob.glob(infiles)
+    rename_files(infiles)
+   
+if __name__ == "__main__":
+    main()
