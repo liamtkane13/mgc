8982f19440bc60d957c57a703c3e69dd7574c8ff
Liam Kane <liamkane@MGC-MACBOOK-PRO-2.local>
2022-10-17 15:08:11 -0400

    adding html

--- utils/samtools_depth_tsv.py
+++ utils/samtools_depth_tsv.py
@@ -28,6 +28,31 @@ def make_name_dictionary():
 
 def run_samtools(files, dictionary):
 
+	html_table = []
+	
+    html_table.append("<head>")
+    intro_line = '<script src="https://code.jquery.com/jquery-3.5.1.js"></script>'
+    intro_line1 = '<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>'
+    intro_line2 = '<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>'
+    link_line1 = '<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">'
+    link_line2 = '<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap.min.css">'
+    html_table.append(link_line1)
+    html_table.append(link_line2)
+    html_table.append(intro_line)
+    html_table.append(intro_line1)
+    html_table.append(intro_line2)
+    html_table.append("</head>")
+    html_table.append("<script>")
+    function = "$(document).ready( function () { \
+        $('#myTable').DataTable(); \
+    } )" 
+
+    html_table.append(function)
+    html_table.append("</script>")	
+
+    header = '<table id="myTable" class="table table-striped table-bordered" style="width:100%"><thead><tr><th>Sample_Name</th><th>Reference_Name</th><th>C10</th><th>C30</th><th>Percent_Bases_Uncovered</th><th>Median_Coverage</th><th>Mean_Coverage</th><th>IGV_Link</th></tr></thead>'
+    html_table.append(header)
+
 	for file in files:
 
 		overall = 0
@@ -35,6 +60,7 @@ def run_samtools(files, dictionary):
 		cov_30 = 0
 		uncov_bases = 0
 		coverage_list = []
+
 
 		barcode_name = file.split('-')[0]
 		sample_name = dictionary[barcode_name]
@@ -82,7 +108,11 @@ def run_samtools(files, dictionary):
 			C30 = 0.0
 
 		if cov_10 > 0:		
-			print(f'{sample_name}\t{ref_name}\t{C10}\t{C30}\t{uncov_percent}\t{median_cov}\t{mean_cov}\t{igv_link}')  
+#			print(f'{sample_name}\t{ref_name}\t{C10}\t{C30}\t{uncov_percent}\t{median_cov}\t{mean_cov}\t{igv_link}')  
+			html_line = (f'<tr><td>{sample_name}</td><td>{ref_name}</td><td>{C10}</td><td>{C30}</td><td>{uncov_percent}</td><td>{median_cov}</td><td>{mean_cov}</td><td><a target="_blank" href="{igv_link}">IGV_Link</a></td></tr>')
+			html_table.append(html_line)
+
+	html_table.append('</table>')		
 
 def main():
 	infiles = args.infiles
