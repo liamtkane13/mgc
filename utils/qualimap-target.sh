1129368ec3093af0b947151e92534ee5e8046d1a
Liam Kane <liamkane@MGC-MACBOOK-PRO-2.local>
2023-01-09 15:34:23 -0500

    lets see

--- utils/qualimap-target.sh
+++ utils/qualimap-target.sh
@@ -6,7 +6,7 @@ for i in `cat test.txt`;
 do 
 aws s3 cp s3://mgcdata/SS2/bams/public/${i}.bam .
 mkdir ${i}/
-echo 'Running QualiMap on ${i}'
-echo 'qualimap --java-mem-size=6G bamqc -bam ${i}.bam -outdir ${i}/ -outfmt HTML -nt 2 --feature-file Agilent-v3-Covered-04-01-2022.bed'	
-echo 'aws s3 cp ${i}/ s3://mgcdata/SS2/qualimap/${i}/'
+echo "Running QualiMap on ${i}"
+echo "qualimap --java-mem-size=6G bamqc -bam ${i}.bam -outdir ${i}/ -outfmt HTML -nt 2 --feature-file Agilent-v3-Covered-04-01-2022.bed"	
+echo "aws s3 cp ${i}/ s3://mgcdata/SS2/qualimap/${i}/"
 done 
