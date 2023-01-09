8928fb7ce2e9a54c4cf06078e828ba3f229e0eb1
Liam Kane <liamkane@MGC-MACBOOK-PRO-2.local>
2023-01-09 16:23:07 -0500

    lets try it out

--- utils/qualimap-target.sh
+++ utils/qualimap-target.sh
@@ -7,7 +7,7 @@ do
 aws s3 cp s3://mgcdata/SS2/bams/public/${i}.bam .
 mkdir ${i}/
 echo "Running QualiMap on ${i}"
-qualimap --java-mem-size=6G bamqc -bam ${i}.bam -outdir ${i}/ -outformat HTML -nt 2 --feature-file Agilent-v3-Covered-04-01-2022.bed	
+qualimap --java-mem-size=6G bamqc -bam ${i}.bam -outdir ${i}/ -outformat HTML -nt 2 --feature-file Agilent-v3-Covered-04-01-2022-6col-qualimap-friendly.bed	
 aws s3 sync ${i}/ s3://mgcdata/SS2/qualimap/${i}/
 rm -r ${i}*
 done 
