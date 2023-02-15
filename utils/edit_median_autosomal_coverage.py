0d5f4a62b7d796b2b2ef7f066b332d56ab1738f9
Liam Kane <liamkane@mgc-mbp-2.mynetworksettings.com>
2023-02-15 14:14:05 -0500

    adding multiple args

--- utils/edit_median_autosomal_coverage.py
+++ utils/edit_median_autosomal_coverage.py
@@ -8,27 +8,29 @@ import os
 
 def parse_arguments():
 	parser = argparse.ArgumentParser(description='Input the JSON Files for correction')
-	parser.add_argument('-i', '--infile', help = 'strainSEEK JSON file for correction', required = True, dest = 'infile')
+	parser.add_argument('-i', '--infile', help = 'strainSEEK JSON file for correction', required = True, nargs = '+', dest = 'infile')
 	args = parser.parse_args()
 	infile = args.infile
 	return infile
 
 
-def edit_json(file):
-	with open(file, 'r') as infile:
-		data = json.load(infile)
-		data["align_metrics_detail"]["Median_autosomal_coverage"] = '0.0'
-		os.remove(file)
-	with open(file, 'w') as new_file:
-		json.dump(data, new_file)
-		new_file.close()	
+def edit_json(files):
+	for file in files:
+		with open(file, 'r') as infile:
+			data = json.load(infile)
+			data["align_metrics_detail"]["Median_autosomal_coverage"] = '0.0'
+			os.remove(file)
+		with open(file, 'w') as new_file:
+			json.dump(data, new_file)
+			new_file.close()	
 
 
 
 def sanity_check(filer):
-	with open(filer, 'r+') as file:
-		data = json.load(file)
-		print(data["align_metrics_detail"]["Median_autosomal_coverage"])
+	for i in filer:
+		with open(i, 'r+') as file:
+			data = json.load(file)
+			print(data["align_metrics_detail"]["Median_autosomal_coverage"])
 
 
 
