7cc01d2c5650d9e78bf7054b5009133274cd8e89
Liam Kane <liamkane@MGC-MACBOOK-PRO-2.local>
2023-03-21 15:45:32 -0400

    working

--- utils/parse_qualimap_cov_tsv.py
+++ utils/parse_qualimap_cov_tsv.py
@@ -9,12 +9,14 @@ import pandas as pd
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Input the Qualimap derived Coverage TSV')
     parser.add_argument('-i', '--infile', help = 'Per Chromosome Coverage TSV', nargs = '+', required = True, dest = 'infile')
+    parser.add_argument('-b', '--batch', help = 'Name of run or batch for output file', required = True, dest = 'batch')
     args = parser.parse_args()
     infile = args.infile
-    return infile
+    batch = args.batch
+    return infile, batch
 
 
-def tsv_crunching(files):
+def tsv_crunching(files, batch):
     data = []
     counter = 0
     for file in files:
@@ -26,12 +28,14 @@ def tsv_crunching(files):
             first_df = pd.concat(data)
         else:
             final_df = pd.merge(first_df, df)
-    print(final_df)          
+    print(final_df)
+    file_name = (f'{batch}_RNASeq_virus_mapping.tsv')
+    final_df.to_csv(file_name, sep='\t')     
 
 
 def main():
-    infile_tsv = parse_arguments()
-    tsv_crunching(infile_tsv)
+    infile_tsv, batch_name = parse_arguments()
+    tsv_crunching(infile_tsv, batch_name)
 
 if __name__ == '__main__':
 	main()	    
