6a002d831e58b4fb4aa77cd031c0adc2b3a4a8f8
Liam Kane <liamkane@mgc-mbp-2.mynetworksettings.com>
2023-05-03 14:53:18 -0400

    good to go!

--- utils/ktImport_on_RSP_samples.sh
+++ utils/ktImport_on_RSP_samples.sh
@@ -4,4 +4,5 @@ for i in `ls *16gb*krona | cut -f 1 -d '-'`;
 do
 ktImportText ${i}*16gb*krona -o ${i}-kraken.html
 aws s3 cp ${i}-kraken.html s3://mgc-minion/mgc_qc/kraken/
+rm ${i}-kraken.html
 done
