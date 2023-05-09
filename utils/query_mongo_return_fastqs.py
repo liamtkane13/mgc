6d64e1861fd6cc1a6a5567c5373579cff21bcaf5
Liam Kane <liamkane@Liam-Kane.local>
2023-05-09 16:52:11 -0400

    trying this now

--- utils/query_mongo_return_fastqs.py
+++ utils/query_mongo_return_fastqs.py
@@ -31,14 +31,14 @@ def connect_to_mongo():
 def query_mongo(rsp, collect):
 
 	for i in collect.find({"_id":rsp}):
-		link_1 = {i['fastq_link'][0]} 
-		link_2 = {i['fastq_link'][1]}
-		print(i['fastq_link'])
-#		processed_link_1 = link_1.split('https://mgcdata.s3.amazonaws.com')[1]
-#		processed_link_2 = link_2.split('https://mgcdata.s3.amazonaws.com')[1]
-#		final_link_1 = ('s3://mgcdata' + processed_link_1)
-#		final_link_2 = ('s3://mgcdata' + processed_link_2)
-#		print(final_link_1, final_link_2)
+		link_1 = i['fastq_link'][0] 
+		link_2 = i['fastq_link'][1]
+#		print(i['fastq_link'])
+		processed_link_1 = link_1.lstrip('https://mgcdata.s3.amazonaws.com') #[1]
+		processed_link_2 = link_2.split('https://mgcdata.s3.amazonaws.com')[1]
+		final_link_1 = ('s3://mgcdata' + processed_link_1)
+		final_link_2 = ('s3://mgcdata' + processed_link_2)
+		print(final_link_1, final_link_2)
 
 def main():
 	infile = parse_arguments()
