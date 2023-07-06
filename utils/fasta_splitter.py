99757f82fba01baf072ece66faaebaca2e43722d
Liam Kane <liamkane@Liam-Kane-2.local>
2023-07-06 15:58:35 -0400

    first commit

---
+++ utils/fasta_splitter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3 
+
+# fasta_splitter.py
+
+from Bio import SeqIO 
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser(description='Input the FASTA file to be split')
+    parser.add_argument('-f', '--file', help = 'FASTA file to butcher', required = True, dest = 'infile')
+	args = parser.parse_args()
+	infile = args.infile 
+	
+	return infile
+
+
+def split_fasta(file):
+	
+	for record in SeqIO.parse(file, 'fasta'):
+		print(record.id)
+
+
+def main():
+	
+	infile = parse_arguments()
+	split_fasta(infile)
+
+
+if __name__ == '__main__':
+	main()			
