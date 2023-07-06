982f894f31075207426464b55065d50aafe0c71b
Stephen McLaughlin <steve.mclaughlin@medicinalgenomics.com>
2023-07-06 21:33:56 +0000

    works!

--- utils/fasta_splitter.py
+++ utils/fasta_splitter.py
@@ -9,22 +9,45 @@ def parse_arguments():
 
 	parser = argparse.ArgumentParser(description='Input the FASTA file to be split')
 	parser.add_argument('-f', '--file', help = 'FASTA file to butcher', required = True, dest = 'infile')
+	parser.add_argument('-t', '--tag', help = 'Tag of interest in Contig Description', required = False, dest = 'tag')
 	args = parser.parse_args()
-	infile = args.infile 
+	infile = args.infile
+	tag = args.tag 
 	
-	return infile
+	return infile, tag 
 
 
-def split_fasta(file):
+def split_fasta(file, tag):
+
 	
 	for record in SeqIO.parse(file, 'fasta'):
-		print(record.id)
+		
+		accession_id = record.id + '.fasta'
+
+		if tag is not None:
+
+			if tag in record.description:
+
+				with open(accession_id, 'w') as outfile:
+					SeqIO.write(record, outfile, "fasta")			
+				outfile.close()
+			
+			else:
+				
+				continue
+
+		else:			
+
+			with open(accession_id, 'w') as outfile:
+				SeqIO.write(record, outfile, "fasta")			
+			outfile.close()		
+
 
 
 def main():
 	
-	infile = parse_arguments()
-	split_fasta(infile)
+	infile, tag = parse_arguments()
+	split_fasta(infile, tag)
 
 
 if __name__ == '__main__':
