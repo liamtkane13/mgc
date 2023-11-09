0dc29e760097dac4d11f44e2d46b23861b69e955
Stephen McLaughlin <steve.mclaughlin@medicinalgenomics.com>
2023-11-09 16:51:32 +0000

    printing dataframe comparisons

--- utils/compare_variants_of_interest.py
+++ utils/compare_variants_of_interest.py
@@ -2,10 +2,13 @@
 
 # compare_variants_of_interest.py
 
+# This script was written to compare variants of RSP 1 with its lower coverage counterpart, RSP 2. As such, this script only checks which variants are in RSP 1, but not RSP 2. Update this for more use cases. 
+
 import argparse
 from pymongo import MongoClient
 import os
 import re 
+import pandas as pd 
 
 def parse_arguments():
 	parser = argparse.ArgumentParser(description='Input BLAST output file, to be filtered by best hit per contig')
@@ -27,4 +30,47 @@ def connect_to_mongo():
 	collection = database['SS2']
 	return collection 
 
-def compare_variants(rsp1, rsp2):
+
+
+def compare_variants(rsp1, rsp2, collect):
+
+	rsp1_list = []
+	rsp2_list = []
+
+	for i in collect.find({'_id':rsp1}):
+
+		rsp1_var = i['kannapedia_variants']
+		
+		for it in rsp1_var:
+			var = it['HGVSc']
+			rsp1_list.append(var)	
+
+	for i in collect.find({'_id':rsp2}):
+
+		rsp2_var = i['kannapedia_variants']
+		
+		for it in rsp2_var:
+			var = it['HGVSc']
+			rsp2_list.append(var)		
+
+	rsp1_df = pd.DataFrame.from_dict(rsp1_var)
+	rsp2_df = pd.DataFrame.from_dict(rsp2_var)
+	print(rsp1_df)
+	print(rsp2_df)
+#	print(rsp1_list)
+#	print(rsp2_list)
+
+	rsp1_only_var = (rsp1_df[~rsp1_df.HGVSc.isin(rsp2_df.HGVSc)])
+	rsp2_only_var = (rsp2_df[~rsp2_df.HGVSc.isin(rsp1_df.HGVSc)])
+	print(rsp1_only_var)
+	print(rsp2_only_var)
+
+
+def main():
+	
+	rsp_1, rsp_2 = parse_arguments()
+	collection = connect_to_mongo()	
+	compare_variants(rsp_1, rsp_2, collection)
+
+if __name__ == '__main__':
+	main()	
